0101aed7c552f8e3e7174203727df0713c5590d4
Neha Pednekar <155717961+Neha-9321@users.noreply.github.com>
2024-07-17 13:59:27 +0530

    Updated Script4.js

--- Script4.js
+++ Script4.js
@@ -1,7 +1,7 @@
 let typed = new Typed(".text", {
-    string: ("Frontend Developer", "Software Intern"),
+    strings: ["Frontend Developer", "Software Intern"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true
-})
+});
